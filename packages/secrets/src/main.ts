ab9d9802641cf28b33bde3f0573bf72a56173dfa
bitomic <35199700+bitomic@users.noreply.github.com>
2023-05-18 23:26:31 -0600

    chore: setup redis

--- packages/secrets/src/main.ts
+++ packages/secrets/src/main.ts
@@ -1,5 +1,22 @@
 import { load } from 'ts-dotenv'
 
 export const env = load( {
-	DISCORD_TOKEN: String
+	DISCORD_TOKEN: String,
+	REDIS_DB: {
+		default: 1,
+		type: Number
+	},
+	REDIS_HOST: String,
+	REDIS_PASSWORD: {
+		default: '',
+		type: String
+	},
+	REDIS_PORT: {
+		default: 6379,
+		type: Number
+	},
+	REDIS_USERNAME: {
+		default: '',
+		type: String
+	}
 } )
